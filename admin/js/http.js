30d4ec0bdb40d2d0aef69f8bc190b70d39666374
yangkaiyu <1264288834@qq.com>
2020-02-29 16:24:15 +0800

    修改

--- admin/js/http.js
+++ admin/js/http.js
@@ -1,24 +1,62 @@
-$(function() {
+(function(w) {
   $.ajaxSetup({
     headers: {
       Authorization: localStorage.getItem("token")
     },
+    // 发送 ajax 请求前 -
     beforeSend: function() {
-      alert("请求前");
+      // 显示进度条
+      // 进度条插件引入后会在 window 对象上添加一个 NProgress 成员，所以才能全局调用
+      // 如果有页面没有引入 NProgress 插件，那 window 对象上就没有 NProgress，那就不调用
+      if (window.NProgress) {
+        NProgress.start();
+      }
     },
     // success:function(){
 
     // },
     error: function() {
       //   alert("登录失效，请重新登陆");
+      //   Bootstrap框架 登录错误弹出框，调用modal()方法
       $(".modal").modal();
       $(".modal p").html("登录失效，请重新登陆");
+      //   当用户点击了去登录跳转到登录页
+      $(".to-login").click(function() {
+        location.href = "./login.html";
+      });
     },
+    // 完成 - 不管成功失败都执行
+    // 请求完成后隐藏进度条
     complete: function() {
-      alert("完成");
+      if (window.NProgress) {
+        // 隐藏进度条
+        NProgress.done();
+      }
     }
   });
-  $(".to-login").click(function() {
-    location.href = "./login.html";
-  });
-});
+
+  const baseUrl = "http://localhost:8080/api/v1";
+  const BigNew = {
+    user_login: `${baseUrl}/admin/user/login`, //用户登录
+    user_info: `${baseUrl}/admin/user/info`, //用户信息
+    user_detail: `${baseUrl}/admin/user/detail`, //用户详情
+    user_edit: `${baseUrl}/admin/user/edit`, //用户编辑
+    category_list: `${baseUrl}/admin/category/list`, //文章类别查询
+    category_add: `${baseUrl}/admin/category/add`, //文章类别新增
+    category_search: `${baseUrl}/admin/category/search`, //文章类别搜索
+    category_edit: `${baseUrl}/admin/category/edit`, //文章类别编辑
+    category_delete: `${baseUrl}/admin/category/delete`, //文章类别删除
+    article_query: `${baseUrl}/admin/article/query`, //文章搜索
+    article_publish: `${baseUrl}/admin/article/publish`, //文章发布
+    article_search: `${baseUrl}/admin/article/search`, //文章信息查询
+    article_edit: `${baseUrl}/admin/article/edit`, //文章编辑
+    article_delete: `${baseUrl}/admin/article/delete`, //文章删除
+    comment_list: `${baseUrl}/admin/comment/search`, //文章评论列表
+    comment_pass: `${baseUrl}/admin/comment/pass`, //文章评论通过
+    comment_reject: `${baseUrl}/admin/comment/reject`, //文章评论不通过
+    comment_delete: `${baseUrl}/admin/comment/delete` //文章评论删除
+  };
+  // window 是实参， w 是函数的形参，保存了 window 对象的地址
+  // 把局部的 BigNew 添加到 window 对象上
+  w.BigNew = BigNew;
+})(window);
--- admin/js/user.js
+++ admin/js/user.js
@@ -6,16 +6,21 @@ $(function() {
     dataType: "json",
     success: function(backData) {
       console.log(backData);
-      //渲染页面
-      // $('input.username').val(backData.data.username);
-      // $('input.nickname').val(backData.data.nickname);
-      // $('input.email').val(backData.data.email);
-      // $('input.password').val(backData.data.password);
-      //遍历对象优化代码
-      for (var key in backData.data) {
-        $("input." + key).val(backData.data[key]);
-      }
-      $("img.user_pic").attr("src", backData.data.userPic);
+      // 渲染页面    // 把用户信息分别写入到表单的 input 标签中
+      $("input.username").val(backData.data.username);
+      $("input.nickname").val(backData.data.nickname);
+      $("input.email").val(backData.data.email);
+      $("input.password").val(backData.data.password);
+      //     //遍历对象优化代码
+      //     for (var key in backData.data) {
+      //       $("input." + key).val(backData.data[key]);
+      //     }
+      //     $("img.user_pic").attr("src", backData.data.userPic);
+      const userPic = backData.data.userPic;
+      // 图片特殊处理，需要通过 src 属性设置图片路径
+      // img.user_pic    img 和 .user_pic 连在一起是交集选择器
+      // 交集选择器：既要求是 img 标签，身上还要求有 .user_pic 的类名
+      $("img.user_pic").attr("src", userPic);
     }
   });
   //2.文件预览
@@ -29,26 +34,70 @@ $(function() {
     $(".user_pic").attr("src", url);
   });
 
-  //3.编辑个人信息(fromdata上传文件)
-  $("#form").on("submit", function(e) {
-    //禁用表单默认提交事件
+  // 3.编辑个人信息(fromdata上传文件)
+  // $("#form").on("submit", function(e) {
+  //   //禁用表单默认提交事件
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: BigNew.user_edit,
+  //     type: "post",
+  //     dataType: "json",
+  //     data: new FormData(this),
+  //     contentType: false,
+  //     processData: false,
+  //     success: function(backData) {
+  //       console.log(backData);
+  //       if (backData.code == 200) {
+  //         alert("修改成功");
+  //         /*
+  //                   window:            当前页面窗口 user.html
+  //                   window.parent:     当前页面父窗口 index.html
+  //                   */
+  //         parent.window.location.reload();
+  //       }
+  //     }
+  //   });
+  // });
+  // 功能3：点击修改按钮，修改了用户信息
+  // 知识点：FormData
+  // FormData 可以把它看做是特殊的参数数据格式。
+  //  在 xhr1 阶段，ajax 常用于提交字符串格式数据。
+  //  在 xhr2 阶段，FormData 可用于图片上传，对象格式。
+  //  用法：new FormData(原生表单域)  可以把表单中的数据自动序列化
+  $(".btn-edit").click(function(e) {
+    // 1、阻止默认行为
     e.preventDefault();
+    // 2、通过 FormData 把表单中的数据自动添加到 FormData 对象中，可直接用于上传
+    // this 代表当前点击的按钮
+    // PS：每个表单元素，都可以通过 this.form 获取它所在的表单域元素
+    const fd = new FormData(this.form);
+    // 3、通过 ajax 把新的用户信息上传到服务器
     $.ajax({
-      url: BigNew.user_edit,
       type: "post",
+      url: BigNew.user_edit,
+      // data 直接写 fd 对象，不要再套上个{}了，{} 是字符串参数的传递
+      data: fd,
+
       dataType: "json",
-      data: new FormData(this),
+      // 不需要要自动添加 contentType 请求头，fd 对象有自己的请求头
       contentType: false,
+      // 不需要把 fd 对象转换成字符串
       processData: false,
-      success: function(backData) {
-        console.log(backData);
-        if (backData.code == 200) {
+      success: function(response) {
+        //  知识点：
+        //    iframe 中的页面，window 对象是指 iframe 页面中的 window 对象
+        //    如果想要获取到 iframe 的父级页面，可以通过 window.parent
+        //    window.parent 要通过 http 协议方式才能正常获取父页
+        if (response.code === 200) {
           alert("修改成功");
-          /* 
-                    window:            当前页面窗口 user.html
-                    window.parent:     当前页面父窗口 index.html
-                    */
-          parent.window.location.reload();
+          //  方案2：通过 window.parent.$() 选中父级页面元素并修改
+          // 获取当前 iframe 中的预览图，和 input.nickname 的值
+          const imgSrc = $("img.user_ipc").attr("scr");
+          const nickname = $("input.nickname").val();
+          // 修改父级页面中的图片
+          // window.parent.$() 操作的就是父级页面的元素
+          window.parent.$(".user_info img,user_center_link img").attr("src");
+          window.parent.$(".user_info strong").html(nickname);
         }
       }
     });
