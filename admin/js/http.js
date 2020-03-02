7ea3d4683bd43331b300990efecd844c2de20ef6
yangkaiyu <1264288834@qq.com>
2020-03-02 16:04:02 +0800

    400和403错误

--- admin/js/http.js
+++ admin/js/http.js
@@ -15,15 +15,28 @@
     // success:function(){
 
     // },
-    error: function() {
-      //   alert("登录失效，请重新登陆");
-      //   Bootstrap框架 登录错误弹出框，调用modal()方法
-      $(".modal").modal();
-      $(".modal p").html("登录失效，请重新登陆");
-      //   当用户点击了去登录跳转到登录页
-      $(".to-login").click(function() {
-        location.href = "./login.html";
-      });
+    // 请求失败，意味服务器拒绝给你数据，造成这个原因往往是用户没有没有正确的 token
+    // 没有正确的 token 就跳到登录页，让用户重新登录并获取 token
+    error: function(xhr) {
+      // alert(xhr);
+      // ajax 请求参数错误的状态码是 400
+      if (xhr.status === 400) {
+        $(".modal").modal();
+        $(".modal p").html("请输入正确的数据");
+        // 进入 index.html 页面的时候还没登录，未登录状态码是 403
+      } else if (xhr.status === 403) {
+        //   alert("登录失效，请重新登陆");
+        //   Bootstrap框架 登录错误弹出框，调用modal()方法
+        $(".modal").modal();
+        $(".modal p").html("登录失效，请重新登陆");
+        //   当用户点击了去登录跳转到登录页
+        // 点击去登录的按钮后跳转到登录页
+        // 请求失败才给按钮注册跳转事件。
+        $(".to-login").click(function() {
+          // 设置为登录页的路径
+          location.href = "./login.html";
+        });
+      }
     },
     // 完成 - 不管成功失败都执行
     // 请求完成后隐藏进度条
